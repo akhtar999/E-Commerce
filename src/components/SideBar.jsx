a36d15af543298d7b854c513a982390ae4dd73e8
Akhtar <iaktr999@gmail.com>
2023-10-22 15:54:06 +0530

    sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useContext } from "react";
+
+import { IoMdArrowForward } from "react-icons/io";
+import { FiTrash2 } from "react-icons/fi";
+import { SidebarContext } from "../contexts/SideBarContext";
 
 const SideBar = () => {
+  const { isOpen, handleClose } = useContext(SidebarContext);
   return (
-    <div>SideBar</div>
-  )
-}
+    <div
+      className={` ${
+        isOpen ? "right-0" : "-right-full"
+      } w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}
+    >
+      <div className="flex items-center justify-between py-6 border-b">
+        <div className="uppercase text-sm font-semibold">Shopping Bag (0)</div>
+        {/* icons */}
+        <div
+          onClick={handleClose}
+          className="cursor-pointer w-8 h-8 flex justify-center items-center"
+        >
+          <IoMdArrowForward className="text-2xl" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SideBar
+export default SideBar;
