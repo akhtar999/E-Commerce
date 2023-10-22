b22acf9899c483c1e3bdbabd5e0e8d8a755f0239
Akhtar <iaktr999@gmail.com>
2023-10-22 15:54:24 +0530

    sidebar context

--- src/contexts/SideBarContext.jsx
+++ src/contexts/SideBarContext.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { createContext, useState } from "react";
 
-const SideBarContext = () => {
+//create context
+export const SidebarContext = createContext();
+
+const SidebarProvider = ({ children }) => {
+  // Sidebar state
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <div>SideBarContext</div>
-  )
-}
+    <SidebarContext.Provider value={{ isOpen, setIsOpen, handleClose }}>
+      {children}
+    </SidebarContext.Provider>
+  );
+};
 
-export default SideBarContext
+export default SidebarProvider;
