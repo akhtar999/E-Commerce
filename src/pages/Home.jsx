8d2df95054e05186ece54463cbc5d6d27ebe4d81
Akhtar <iaktr999@gmail.com>
2023-10-22 11:31:53 +0530

    getting product from context and displaying

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useContext } from "react";
+// import product context
+import { ProductContext } from "../contexts/ProductContext";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  // get product from product context
+  const products = useContext(ProductContext);
 
-export default Home
+  // get only men's and women's clothings
+  const filterProducts = products.filter((item) => {
+    return (
+      item.category === "men's clothing" || item.category === "women's clothing"
+    );
+  });
+  // console.log(filterProducts);
+  // console.log(products);
+  return (
+    <div>
+      <section className="py-16">
+        <div className="container mx-auto">
+          <div>
+            {filterProducts.map((product) => {
+              return <div key={product.id}>{product.title}</div>;
+            })}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
