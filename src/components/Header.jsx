c0cf3e851ccab9e623117af7a20f873f9f5e005c
Akhtar <iaktr999@gmail.com>
2023-10-22 15:52:56 +0530

    js function implemented

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { SidebarContext } from "../contexts/SideBarContext";
+
+import { BsBag } from "react-icons/bs";
 
 const Header = () => {
+  const { isOpen, setIsOpen } = useContext(SidebarContext);
   return (
-    <div>Header</div>
-  )
-}
+    <header className="bg-orange-400">
+      <div>Header</div>
+      <div
+        onClick={() => setIsOpen(!isOpen)}
+        className="cursor-pointer flex relative"
+      >
+        {<BsBag className="text-2xl" />}
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
